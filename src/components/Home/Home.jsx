156be6f6d848be5ae9966fdd47c47dc79806d86a
parthbhatt268 <parthbhatt268@gmail.com>
2025-07-12 13:53:17 +0100

    rotator text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import '../Effects/RotatingText/RotatingText.css';  // bring in its styles
 import './Home.css';
+import RotatingText from '../Effects/RotatingText/RotatingText';
 import desktopBg from '../../assets/images/desktop-bg-half.png';
 
 const Home = () => {
@@ -10,10 +12,27 @@ const Home = () => {
         alt="Hero"
         className="hero__img"
       />
-      <div className="hero__content">
-        <h1>Home</h1>   
+
+      {/* RotatingText from your local Effects folder */}
+      <div className="hero__rotator">
+        <RotatingText
+          texts={['React', 'Bits', 'Is', 'Cool!']}
+          mainClassName="rotating-text"
+          staggerFrom="last"
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="rotating-text-split"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={2000}
+        />
       </div>
-    </div>  
+
+      <div className="hero__content">
+        <h1>Home</h1>
+      </div>
+    </div>
   );
 };
 
