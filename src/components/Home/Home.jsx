4aa4efc8d7de734aad57311c8e32cc699c636aed
parthbhatt268 <parthbhatt268@gmail.com>
2025-07-19 22:53:06 +0100

    connect with me button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,10 @@ import desktopBg from '../../assets/images/desktop-bg-half.png';
 const roles = ['software', 'full-stack', 'frontend', 'backend'];
 
 const Home = () => {
+  const handleConnectClick = () => {
+    window.open('https://www.linkedin.com/in/parth-bhatt-092024186/', '_blank');
+  };
+
   return (
     <div className="hero" role="img" aria-label="Background image of …">
       {/* background image layer */}
@@ -14,7 +18,7 @@ const Home = () => {
 
       {/* your intro text with inline rotating roles */}
       <div className="hero__content">
-        <h1>Hi, I’m Parth,</h1>
+        <h1>Hi, I'm Parth,</h1>
         <p className="hero__subtitle">
           an Experienced{' '}
           <RotatingText
@@ -31,6 +35,17 @@ const Home = () => {
           />{' '}
           Developer, based in Galway, Ireland.
         </p>
+        
+        {/* Connect with me button */}
+        <button className="connect-button" onClick={handleConnectClick}>
+          <svg className="linkedin-icon" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+          </svg>
+          <span>Connect with me</span>
+          <svg className="external-icon" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14L21 3"/>
+          </svg>
+        </button>
       </div>
     </div>
   );
