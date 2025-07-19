c22cdbd9b12f1ffd4304a4fd966f59dde82ada7e
parthbhatt268 <parthbhatt268@gmail.com>
2025-07-19 23:48:07 +0100

    responsive hoeme page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import '../Effects/RotatingText/RotatingText.css';
 import './Home.css';
 import RotatingText from '../Effects/RotatingText/RotatingText';
 import desktopBg from '../../assets/images/desktop-bg-half.png';
+import profileImage from '../../assets/images/mobile-bg.png';
 
 const roles = ['software', 'full-stack', 'frontend', 'backend'];
 
@@ -33,8 +34,16 @@ const Home = () => {
             transition={{ type: 'spring', damping: 30, stiffness: 400 }}
             rotationInterval={2000}
           />{' '}
-          Developer, based in Galway, Ireland.
+          Developer, <br/>
+          based in Galway, Ireland.
         </p>
+        
+        {/* Profile Card */}
+        <div className="profile-card">
+          <div className="profile-image">
+            <img src={profileImage} alt="Parth Bhatt" />
+          </div>
+        </div>
         
         {/* Connect with me button */}
         <button className="connect-button" onClick={handleConnectClick}>
@@ -42,9 +51,6 @@ const Home = () => {
             <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
           </svg>
           <span>Connect with me</span>
-          <svg className="external-icon" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14L21 3"/>
-          </svg>
         </button>
       </div>
     </div>
