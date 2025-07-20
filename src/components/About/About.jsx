c8eed0f5b48f13a11468231a2771d7bc807d6b56
parthbhatt268 <parthbhatt268@gmail.com>
2025-07-20 22:08:08 +0100

    profile card

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import './About.css';
+// src/components/About/About.jsx
+import React from "react";
+import "./About.css";
+import parthPhoto from "../../assets/images/Parth1.png";
+
+import ProfileCard from "../Effects/ProfileCard/ProfileCard";
 
 const About = () => {
+  const text =
+    "Hi,👋 my name is Parth and I am a Full stack Web Developer. " +
+    "I’m currently pursuing my Master's Degree 🎓 in Information System Management (ISM) " +
+    "with the aim of achieving a 1:1 🎖 from University of Galway🏛. " +
+    "You can connect with me on the following social media platforms. " +
+    "Also, if you have any suggestions or comments for the portfolio you can connect with me on LinkedIn. " +
+    "Thank you for visiting my portfolio. 😊";
+
   return (
-    <div className="about-container">
+    <section className="about-container">
       <h2 className="about-title">About</h2>
       <div className="about-content">
-        <p>Information about me and my background will be displayed here.</p>
+        {/* left: card */}
+        <div className="about-profile">
+          <ProfileCard
+            showUserInfo={true}
+            showBehindGradient={true}
+            enableTilt={true}
+            name="Parth Bhatt"
+            title="Software Engineer"
+            handle="parthbhatt268"
+            status="Online"
+            contactText="Contact Me"
+            onContactClick={() => console.log("Contact clicked")}
+          />
+        </div>
+
+        {/* right: text */}
+        <div className="about-text">
+          <p>{text}</p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default About; 
+export default About;
